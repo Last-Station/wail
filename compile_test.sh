dc295d065ea14947d7a356502abc0c4fe59e0450
Miko <tarmiko2025@gmail.com>
2025-12-30 21:06:53 +0700

    .

---
+++ compile_test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# For now this custom build script should be good enough
+
+# People like colors
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+# Yes, this is all kinda hardcoded, live with it.
+echo -e "${YELLOW}Removing old build.${NC}"
+rm -r build
+
+echo -e "${YELLOW}Creating build directory.${NC}"
+mkdir -p build/resources/font/Noto_Sans
+
+# Exit on error
+set -e
+
+echo -e "${YELLOW}Creating file links.${NC}"
+
+ln -s '../../../../resources/font/Noto_Sans/NotoSans-Italic-VariableFont_wdth,wght.ttf' \
+    build/resources/font/Noto_Sans/Default_Font.ttf
+
+echo -e "${YELLOW}Compiling resources.${NC}"
+
+ld -r -b binary build/resources/font/Noto_Sans/Default_Font.ttf \
+ -o build/resources/Default_Font.ttf.o
+
+echo -e "${YELLOW}Compiling executables.${NC}"
+
+g++ \
+ -ISDL3/include/ -ISDL3_ttf/include/ -ISDL_image/include/ -lSDL3_image -lSDL3_ttf -lSDL3 \
+ -Wall -Wextra -pedantic \
+ -c src/gui/window.cpp \
+ -o build/window.o
+
+g++ \
+ -ISDL3/include/ -ISDL3_ttf/include/ -ISDL_image/include/ -lSDL3_image -lSDL3_ttf -lSDL3 \
+ -c src/gui/HSButton.cpp -o build/HSButton.o \
+ -Wall -Wextra -pedantic \
+
+g++ -c src/main.cpp -o build/main.o \
+ -Wall -Wextra -pedantic \
+ -ISDL3/include/ -ISDL3_ttf/include/ -ISDL_image/include/ -lSDL3_image -lSDL3_ttf -lSDL3
+
+echo -e "${YELLOW}Basically linking everything together at this point!${NC}"
+
+cd build
+ln -s ../src/res res
+
+g++ *.o resources/*.o -o test_build \
+ -ISDL3/include/ -ISDL3_ttf/include/ -ISDL_image/include/ -L../../sdl_img_build/ -lSDL3_image -lSDL3_ttf -lSDL3
+
+echo -e "${GREEN}Done!${NC}"
+
+exit
